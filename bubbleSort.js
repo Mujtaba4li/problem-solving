e7f874e4a3d11dcfcb055fbf9ebf6ca7eddde02f
Mujtaba Ali <mujiroy01@gmail.com>
2023-01-07 22:13:41 +0500

    bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,17 @@
-for(let i=1;i<5;i++)
-{
-let a=i;
---a;
-console.log("a :"+a+" and i :"+i);
+function bubbleSort(array) {
+    for (let i = array.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+            }
+        }
 
+
+    }
+    return array;
 }
+// let arr=[5,3,2,5,2,6,8,6,1,3];   
+const res = bubbleSort([5, 3, 2, 5, 2, 6, 8, 6, 1, 3]);
+console.log(res);
+
+
--- tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-
-// console.log(fibonacci(2));  // 1
-// console.log(fibonacci(3));  // 2
-// console.log(fibonacci(4));  // 3
-// console.log(fibonacci(5));  // 5
